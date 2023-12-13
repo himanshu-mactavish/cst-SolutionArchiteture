a3d8ba2da03cd2e5a54c9ea27f34bf3494b6b7db
Himanshu MacTavish <Himanshu.Mactavish@gmail.com>
2023-12-13 13:16:31 -0500

    req:testing

---
+++ src/config.py
@@ -0,0 +1,37 @@
+"""
+
+
+Created and Maintained by Himanshu MacTavish(https://github.com/himanshu-mactavish)
+
+
+
+This file mainly focuses on creating configurations
+file for easy setup of Colossus.
+
+This must be the first file that you edit and run in
+order to generate configurations.ini file for
+improving security and easy setup.
+
+Required fields -> sender_address, receiver_address, file_location, mailtrap_user, mailtrap_password
+
+"""
+
+import configparser
+
+config_file = configparser.ConfigParser()
+config_file.add_section("SMTPlogin")
+
+# Add settings to section
+# Here you need to modify the values of last parameter of set function
+config_file.set("SMTPlogin", "sender_address", "----")
+config_file.set("SMTPlogin", "receiver_address", "----")
+config_file.set("SMTPlogin", "mailtrap_user", "----")
+config_file.set("SMTPlogin", "mailtrap_password", "----")
+
+# Saving config file as configurations.ini
+with open(r"configurations.ini", 'w') as configfileObj:
+    config_file.write(configfileObj)
+    configfileObj.flush()
+    configfileObj.close()
+
+print("Config file 'configurations.ini' created")
---
+++ src/main.py
@@ -0,0 +1,63 @@
+"""
+Created and Maintained by Himanshu MacTavish(https://github.com/himanshu-mactavish)
+
+"""
+import getopt, sys
+import hybrid
+import uploadS3
+import os
+import boto3
+import decrypt
+
+def main():
+    bucket = None
+    object = None
+    src = None
+    type = None
+
+    argv = sys.argv[1:]
+
+    try:
+        opts, args = getopt.getopt(argv, "b:o:i:t:h:")
+
+    except:
+        print ("usage: python3 main.py -t upload -b <bucket-name> -o <object> -i <image-name>")
+        print ("usage: python3 main.py -t download -b <bucket-name> -o <object> -i <image-name>")
+        print("usage: python3 main.py -t decrypt -i <image-name>")
+        sys.exit(2)
+
+    else:
+        for opt, arg in opts:
+            if opt in ['-h']:
+                print ("usage: python3 main.py -t upload -b <bucket-name> -o <object> -i <image-name>")
+                print ("usage: python3 main.py -t download -b <bucket-name> -o <object> -i <image-name>")
+                print ("usage: python3 main.py -t decrypt -i <image-name>")
+                os._exit(0)
+
+            elif opt in ['-t']:
+                type = arg
+            elif opt in ['-b']:
+                bucket = arg
+            elif opt in ['-o']:
+                object = arg
+            elif opt in ['-i']:
+                src = arg
+        if (type == "upload"):
+            hybrid.mainMenu()
+            uploadS3.upload_file(src, bucket, object)
+            print("Uploaded Successfully!!!")
+
+        elif (type == "download"):
+            s3 = boto3.client('s3')
+            s3.download_file(bucket, object, src)
+            print("File Downloaded!!!")
+
+        elif (type == "decrypt"):
+            decrypt.main(src)
+
+if __name__ == '__main__':
+    main()
+
+# Test
+# python main.py -t upload -b sefy0 -i Capture1.PNG
+# python main.py -t download -b sefy0 -o Capture1.PNG -i Capture1.PNG
